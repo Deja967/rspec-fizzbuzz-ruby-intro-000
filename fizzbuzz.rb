d8f3b3c097e26c6de81e6e4adbd36772009c62d0
Deja Monae Allison <dejaallison@gmail.com>
2020-06-29 14:55:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  if int % 3 & 5 == 0
+    puts "fizzbuzz"
+  end
   if int % 3 == 0
     puts "Fizz"
   end
   if int % 5 == 0
     puts "Buzz"
   end
-  if int % 3 & 5 == 0
-    puts "fizzbuzz"
-  end
+
 end
